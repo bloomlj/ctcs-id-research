3a8b0296a117b6364a1c80bb0978368920c76d39
Lee Bloom <kollocc@live.com>
2020-11-27 16:53:52 +0800

    first version
    单个按钮测试完毕，使用了两个leonardo

---
+++ openrails/ButtonSerial/ButtonSerial.ino
@@ -0,0 +1,18 @@
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+  // initialize digital pin LED_BUILTIN as an output.
+  Serial1.begin(9600);
+  Serial.begin(9600);
+  pinMode(7,INPUT_PULLUP);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  int x = digitalRead(7);
+  if(!x){
+    Serial1.println('A');
+    Serial.println(x);
+  }
+  delay(500);
+}
---
+++ openrails/KeyboardSerial/KeyboardSerial.ino
@@ -0,0 +1,37 @@
+/*
+  Keyboard Serial
+  该程序用于将控制台的按钮信号转换为openrails的快捷键
+  Openrails机车控制默认快捷键：
+  机车向前：W
+  倒退：S
+  功率+：D
+  功率-：A
+  加档：E
+  减档：Shift+E
+  紧急制动：Back
+  鸣笛：Space
+  左边车门：Q
+  右边车门：Shift+Q
+
+*/
+
+#include "Keyboard.h"
+
+void setup() {
+  // open the serial port:
+  Serial.begin(9600);
+  Serial1.begin(9600);
+  // initialize control over the keyboard:
+  Keyboard.begin();
+}
+
+void loop() {
+  // check for incoming serial data:
+  if (Serial1.available() > 0) {
+    // read incoming serial data:
+    char inChar = Serial1.read();
+    // Type the next ASCII value from what you received:
+   Keyboard.write(inChar);
+   //Serial.println(inChar);
+  }
+}
